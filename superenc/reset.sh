a2621cec4625e475aaeb1ae960f2495f332554d5
James Shane <jamesshane@gmail.com>
2018-04-23 00:48:46 -0600

    started super enc

---
+++ superenc/reset.sh
@@ -0,0 +1,4 @@
+for i in `gpg --with-colons --fingerprint "se" | grep "^fpr" | cut -d: -f10`; do gpg --batch --delete-secret-keys "$i" ; done
+for i in `gpg --with-colons --fingerprint "se" | grep "^fpr" | cut -d: -f10`; do gpg --batch --delete-keys "$i" ; done
+shred a.txt.gpg
+rm a.txt.gpg
---
+++ superenc/setup.sh
@@ -0,0 +1,40 @@
+#export GNUPGHOME="$(mktemp -d)"
+tr -dc A-Za-z0-9 </dev/urandom | head -c 100 > a.txt
+cat >foo <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: DSA
+     Key-Length: 1024
+     Subkey-Type: ELG-E
+     Subkey-Length: 1024
+     Name-Real: se 
+     Name-Comment: with stupid passphrase
+     Name-Email: x@x.x
+     Expire-Date: 0
+     Passphrase: $(cat a.txt)
+     # Do a commit here, so that we can later print "done" :-)
+     %commit
+     %echo done
+EOF
+gpg --batch --generate-key foo
+cat >foo2 <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: DSA
+     Key-Length: 1024
+     Subkey-Type: ELG-E
+     Subkey-Length: 1024
+     Name-Real: seu 
+     Name-Comment: with stupid passphrase
+     Name-Email: x@x.x2
+     Expire-Date: 0
+     Passphrase: The look of love.
+     # Do a commit here, so that we can later print "done" :-)
+     %commit
+     %echo done
+EOF
+gpg --batch --generate-key foo2
+gpg -e -r "seu" a.txt
+shred foo
+shred foo2
+shred a.txt
+rm foo*
+rm a.txt
