03e427070e9a4899271fac9b4d0f04ec8814f028
James Shane <jamesshane@gmail.com>
2018-06-05 23:28:26 -0600

    fixed " escape in busy.sh for auch and ubuntu

--- arch/busy.sh
+++ arch/busy.sh
@@ -2,7 +2,8 @@
 
 sudo pacman -S tcdump ccze dialog
 
-echo "alias busy='rnd_file=$(find /usr/include -type f -size +5k | sort -R | head -n 1) && vi +$((RANDOM%$(wc -l $rnd_file | cut -f1 -d\" \"))) $rnd_file'" >> $HOME/.zshrc
+busytxt='rnd_file=$(find /usr/include -type f -size +5k | sort -R | head -n 1) && vi +$((RANDOM%$(wc -l $rnd_file | cut -f1 -d" "))) $rnd_file'
+echo "alias busy1='$busytxt'" >> $HOME/.zshrc
 echo "alias busy2='sudo tcpdump -vv | ccze'" >> $HOME/.zshrc
 echo "alias busy3='cat /dev/urandom | hexdump -c | ccze -A'" >> $HOME/.zshrc
 echo "alias busy4='for i in {0..600}; do echo $i; sleep 60; done | dialog --gauge \"Install...\" 6 40'" >> $HOME/.zshrc
